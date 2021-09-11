45b9c70d74d95e2dae1f6697f89179f18630eac7
so yeon <alicesysy@swu.ac.kr>
2021-09-12 00:23:53 +0900

    [feat] 앱 실행마다 인증 요청

--- KakaoQR/KakaoQR/AppDelegate.swift
+++ KakaoQR/KakaoQR/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -30,7 +30,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+//        if let currentVC = UIApplication.topViewController() as? QRViewVC {
+//            currentVC.dismiss(animated: false, completion: nil)
+//        }
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        let rootVC = LoginVC()
+        window?.rootViewController = rootVC
+        window?.makeKeyAndVisible()
+    }
 }
 
---
+++ KakaoQR/KakaoQR/Resources/Extensions/UIApplication+.swift
@@ -0,0 +1,25 @@
+//
+//  UIApplication+.swift
+//  KakaoQR
+//
+//  Created by soyeon on 2021/09/12.
+//
+
+import UIKit
+
+extension UIApplication {
+    class func topViewController(base: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+        if let nav = base as? UINavigationController {
+            return topViewController(base: nav.visibleViewController)
+        }
+        if let tab = base as? UITabBarController {
+            if let selected = tab.selectedViewController {
+                return topViewController(base: selected)
+            }
+        }
+        if let presented = base?.presentedViewController {
+            return topViewController(base: presented)
+        }
+        return base
+    }
+}
