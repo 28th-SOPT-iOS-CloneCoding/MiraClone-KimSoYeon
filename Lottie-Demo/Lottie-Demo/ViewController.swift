8d3e332e4b1b9f59dc56e5c76568479217ec72ac
so yeon <alicesysy@swu.ac.kr>
2021-10-03 01:15:14 +0900

    [add] 프로젝트 파일 추가

---
+++ Lottie-Demo/Lottie-Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lottie-Demo
+//
+//  Created by soyeon on 2021/10/03.
+//
+
+import UIKit
+import Lottie
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
